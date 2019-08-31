535c0e9d07de982fd1ffc74a02e1fe1bdc0f6baf
manntrix <MANISH.MANDAL21@GMAIL.COM>
2019-09-01 00:31:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# wp-commandline
+
+This will install wp-cli in your machine.
+
+Now you no need to worry how to install wp-cli in your machine. If nodejs is intsalled in your machine then all you need to do is 
+
+```bash
+npm install wp-commandline -g
+```
+
+This will install all wp-cli commands in your terminal. The only thing requires is nodejs, php and mysql installed in your machine.
+
+### Credits
+All the contributors of [WP-CLI](https://github.com/wp-cli/wp-cli) 
---
+++ bin/index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+const sh = require('shelljs')
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+ 
+updateNotifier({pkg}).notify();
+
+if (!sh.which('php')) {
+    shell.echo('Sorry, this module requires php to be installed in your machine or check your environment variable.');
+    shell.exit(1);
+  }
+  else{
+    var args = process.argv.slice(2)
+    var option = '';
+    
+    for(i=0; i< args.length; i++){
+        option += args[i] + " ";
+    }
+    
+    var readStream = `${__dirname}/wp-cli.phar`
+    
+    sh.chmod(755, readStream)
+    
+    sh.exec(`php ${readStream} ${option}`)
+  }
+
